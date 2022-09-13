f7453a74a1ad7df44cbdc2f47df3361c0c8fccff
Austin Erickson <erickson656@hotmail.com>
2022-09-13 13:27:36 -0400

    tags and height fix
    added generic red/green enemy/friend tag prefabs that are dynamically instantiated along with the virtual humans.
    
    Fixed height of spawnPoint boxes to be on ground plane.
    
    Added LookAt() code to virtual humans and tags to always face the main camera.
    
    Adjusted layer settings of virtual humans to be visible on HL.
    
    Added toggle switch on Spawner.cs to control visibility of virtual humans on HoloLens (independent from tag visibility).
    
    In order for this to match the main Husis scene, we just need to adjust the spawn point positions in the other unity project to match the ones here (otherwise the height will be slightly off).

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,9 +9,34 @@ public class Spawner : TargetGenerator
     public List<PointingTaskTarget> Enemies;
     public List<PointingTaskTarget> Friends;
     public Transform[] spawnPoints;
+    public GameObject friendTag;
+    public GameObject enemyTag;
+
+    public bool avatarsOnHL = true;
+    private bool previousSetting = true;
+    private List<GameObject> virtualHumans;
 
     private List<PointingTaskTarget> spawnedTargets;
-  
+
+    // quick function to turn on avatars on the HL for alignment purposes
+    public void SetHumanVisibility(bool visible)
+    {
+        foreach(var vh in virtualHumans)
+        {
+            vh.gameObject.SetActive(visible);
+        }
+    }
+
+    // check the state of the human visibility toggle, update layers if needed
+    private void Update()
+    {
+        if(previousSetting != avatarsOnHL)
+        {
+            SetHumanVisibility(avatarsOnHL);
+            previousSetting = avatarsOnHL;
+        }
+    }
+
     public override void GenerateTargets(int totalEnemies, int totalFriends, ref List<PointingTaskTarget> enemies, ref List<PointingTaskTarget> friends)
     {
         if (spawnPoints.Length < (Enemies.Count + Friends.Count))
@@ -27,19 +52,41 @@ public class Spawner : TargetGenerator
         friends = new List<PointingTaskTarget>();
         var shuffledSpawnPoints = spawnPoints.OrderBy(a => Guid.NewGuid()).ToList();
         int idx = 0;
-
+        virtualHumans = new List<GameObject>();
+        Transform camT = Camera.main.transform;
         foreach (var enemyPrefab in Enemies)
         {
             PointingTaskTarget t = Instantiate(enemyPrefab, shuffledSpawnPoints[idx].position, shuffledSpawnPoints[idx].rotation);
+            
+            t.transform.LookAt(new Vector3(camT.position.x,t.transform.position.y,camT.position.z));
             enemies.Add(t);
             idx++;
+            for (int i = 0; i < t.transform.childCount; i++)
+            {
+                if (t.transform.GetChild(i).name.Contains("bones"))
+                {
+                    virtualHumans.Add(t.transform.GetChild(1).gameObject);
+                    Instantiate(enemyTag, t.transform);
+                    break;
+                }
+            }
         }
 
         foreach (var friendPrefab in Friends)
         {
             PointingTaskTarget t = Instantiate(friendPrefab, shuffledSpawnPoints[idx].position, shuffledSpawnPoints[idx].rotation);
+            t.transform.LookAt(new Vector3(camT.position.x, t.transform.position.y, camT.position.z));
             friends.Add(t);
             idx++;
+            for (int i = 0; i < t.transform.childCount; i++)
+            {
+                if (t.transform.GetChild(i).name.Contains("bones"))
+                {
+                    virtualHumans.Add(t.transform.GetChild(1).gameObject);
+                    Instantiate(friendTag, t.transform);
+                    break;
+                }
+            }
         }
     }
 
